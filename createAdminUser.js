6d6299d1c7e9cdb6ba209dcffeb0a88c344eb67e
brunosiqueira <bsiqueira@gmail.com>
2014-10-15 15:42:10 -0300

    updateing script for user admin

--- createAdminUser.js
+++ createAdminUser.js
@@ -1,9 +1,15 @@
-var groupName = "Cape Town", admin = true, userName = 'admin',userEmail = 'user@email.com', userPass = 'admin';
+var groupName = "Cape Town",
+    admin = true,
+    userName = 'admin',
+    userEmail = 'user@email.com',
+    userPass = 'admin'
+    latitude = -33.920684,
+    longitude = 18.425690;
 
 var db = require('./lib/db');
 
 
-group = db.Group.build({name: groupName, isAdmin: admin, lat: -33.920684, lng: 18.425690 });
+group = db.Group.build({name: groupName, isAdmin: admin, lat: latitude, lng: longitude });
 group.save().success(function(group){
     var user = db.User.build({
         username : userName,
