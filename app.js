13ccab209d2df9cfec433086bed014e4fc4432dc
brunosiqueira <bsiqueira@gmail.com>
2014-10-15 16:07:36 -0300

    Improving streaming socket

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express')
   , http = require('http')
   , app = express()
   , server = http.createServer(app)
-  , io = require('socket.io').listen(server)
+  , io = require('socket.io').listen(server, {origins:'*:*'})
   , passport = require('passport')
   , db = require('./lib/db')
   , config = require('./lib/config');
@@ -14,9 +14,10 @@ var express = require('express')
 // Express configuration
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
